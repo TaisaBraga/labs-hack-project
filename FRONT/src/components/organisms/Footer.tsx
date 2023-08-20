97d7bad9d6f0940485cca6f303c30f68b347e066
Rebeca Mello <rebecammsilva@outlook.com>
2023-08-20 11:33:46 -0300

    create footer

--- FRONT/src/components/organisms/Footer.tsx
+++ FRONT/src/components/organisms/Footer.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import bagLogo from '../../images/bag-logo.png'
+import { makeStyles } from '@mui/styles'
 
+const useStyles = makeStyles(() => ({
+  Container: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: '100%',
+    position: 'absolute',
+    backgroundColor: '#7FB77E',
+    '& img': {
+      width: '84px',
+      paddingTop: '10px',
+    },
+    '& p': {
+      marginTop: '0',
+    }
+}
+}))
 export const Footer = () => {
+  const classes = useStyles()
   return (
-    <div>Footer</div>
+    <div>
+      <div className={classes.Container}>
+        <img src={bagLogo} alt="Sacola da Logo" />
+        <p>Todos os direitos reservados. ©</p>
+      </div>
+    </div>
   )
 }
