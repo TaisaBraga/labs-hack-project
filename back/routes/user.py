3b0e580e3bfe92526546271b3c309c3b3c0435aa
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2023-08-20 16:30:43 -0300

    add patch user

--- back/routes/user.py
+++ back/routes/user.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import IntegrityError
 from back.database.database import get_db
+from back.OAuth2.OAuth2 import current_User
 from back.database import models
 from back import schemas
 from back import utils
@@ -13,8 +14,17 @@ def get_all(db: Session = Depends(get_db)):
     users = db.query(models.User).all()
     return {"message": users}
 
+@router.get("/{id}")
+def get_user_by_id(id: str, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=404, detail=f"Fornecedor não encontrado"
+        )
+    return { "user": user}
 
-@router.post('/register')
+
+@router.post('/register', status_code=201)
 def register_user(user: schemas.UserEntry, db: Session = Depends(get_db)):
     password_hashed = utils.hash(user.password)
     user.password = password_hashed
@@ -28,3 +38,19 @@ def register_user(user: schemas.UserEntry, db: Session = Depends(get_db)):
         return {'message': return_user}
     except IntegrityError as err:
         raise HTTPException(status_code=409, detail={'message': err.args})
+
+
+@router.patch("/{id}", status_code=202)
+def update_user(user_id: str, user: schemas.UserUpdate, db: Session = Depends(get_db), current_user: int= Depends(current_User)):
+    user_query = db.query(models.User).filter(models.User.id == user_id)
+    if current_user.email != user_query.first().email:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={'message': 'Operação não autorizada'}
+        )
+    
+    data_dict = user.model_dump(exclude_unset=True)
+    user_query.update(data_dict)
+    db.commit()
+    
+    return {"message": "updated"}
