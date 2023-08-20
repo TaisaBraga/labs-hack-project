1640b5404bb0a7d3b561713275df115f5ecaf56c
Rebeca Mello <rebecammsilva@outlook.com>
2023-08-20 10:32:02 -0300

    create about us page

--- FRONT/src/components/views/HomeView.tsx
+++ FRONT/src/components/views/HomeView.tsx
@@ -4,12 +4,6 @@ import { AboutUs } from "../organisms/AboutUs"
 export const HomeView = () => {
   return (
     <div>
-      <h1 style={{
-        color: '#1E1E1E',
-        fontFamily: 'Secular One, sans-serif',
-        fontSize: '5em',
-        textAlign: 'center'
-      }}>Good2Go</h1>
       <AboutUs />
       <ProductsList />
     </div>
